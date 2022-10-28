11c2f5c9bf6f7d4d1d95244d34fc3c0d927f4b37
Amelfi <amelfi89@gmail.com>
2022-10-28 16:40:38 -0400

    feature pagination

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import { useDispatch } from 'react-redux';
 
 export default function BasicPagination() {
+  // activar cuando este listo
+//  let dispatch = useDispatch()
+  const handleChange=(e, p)=>{
+    console.log(e,p)
+    // activar cuando este listo
+    // dispatch(pages(p))
+  }
   return (
     <Stack spacing={2}>
-      <Pagination count={10} color="primary" />
+      <Pagination count={10} color="primary" onChange={handleChange}/>
     </Stack>
   );
 }
