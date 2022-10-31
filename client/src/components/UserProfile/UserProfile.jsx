c02e185a2fd64840fdef7ed1046a91f4b600a024
Franco Arenas <franco.argenmu.35@gmail.com>
2022-10-31 05:10:24 -0300

    retoquesProfile

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,5 +1,4 @@
 import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import NavbarP from '../NavbarP/NavbarP';
 import { useAuth0 } from "@auth0/auth0-react";
@@ -18,36 +17,34 @@ export default function UserProfile() {
  {
     
 user && 
+<div>
+
+<div className='name'><Typography variant="h3" gutterBottom>{user.name}</Typography>
+<div className='edit'>
+<Button variant="outlined">EDITAR PERFIL</Button>
+</div>
+</div>
 <div className='conteiner'>
 
-<Avatar
-  alt="Remy Sharp"
-  src={user.picture}
-  sx={{ width: 250, height: 250 }}
-/>
-
-<div className='seguidores'><Typography variant="h4" gutterBottom>{user.name}</Typography>
-</div>
+<img className='avatar' src={user.picture}/>
 
 <div className='seguidores'><Typography variant="h6" gutterBottom>FOLLOWING</Typography>
-<Typography className='contador' variant="h6" display="block" gutterBottom>115</Typography>
+<Typography className='contador' variant="h7" display="block" gutterBottom>115</Typography>
 </div>
 
 <div className='seguidores' ><Typography variant="h6" gutterBottom>FOLLOWERS</Typography>
-<Typography className='contador' variant="h6" display="block" gutterBottom>1.000.053</Typography>
+<Typography className='contador' variant="h7" display="block" gutterBottom>1.000.053</Typography>
 </div>
 
-<div className='seguidores' >
-<Button variant="outlined">EDITAR PERFIL</Button>
-</div>
 
+</div>
 </div>
  }
-
  <br/>
  <br/>
- 
+ <div className='list'>
 <ListFav/>
+</div>
          </div>
         )
     }
