09008ef7343e4dc07c77ebcf81f0f96b13f9a551
Fedegz1999 <federicoguti@gmail.com>
2022-10-30 21:19:55 -0300

    a

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,23 +1,80 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import './Profile.jsx'
+import './Login.css'
 
 export default function Login(){
+  
 
-    const {loginWithRedirect,logout,isAuthenticated} = useAuth0()
+  
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  const {loginWithRedirect,logout,isAuthenticated} = useAuth0()
 
     return(
           <div>
              {
               !isAuthenticated ?
-            <Button variant="text" color="rojo" className="botones" onClick={() => loginWithRedirect()}>
+              <div>
+            <Button variant="text" color="rojo" className="botones2" onClick={() => loginWithRedirect()}>
               Login
             </Button>
-            :
-            <Button variant="text" color="rojo" className="botones" onClick={() => logout()}>
-            Logout
+            <Button variant="contained" color="rojo" className="botones2">
+            Register
           </Button>
+          </div>
+            :
+            <div className='botonesDiv'>
+
+
+          <Button variant="text" color="amarillo" className="botones4" >
+        Premium
+          </Button>
+
+          <Button variant="text" color="rojo" className="botones" >
+        Compare
+          </Button>
+
+          <Button variant="text" color="rojo" className="botones" >
+          Watchlist
+          </Button>
+
+          <Button variant="text" color="rojo" className="botones" >
+          List
+          </Button>
+
+          <Button variant="text" color="rojo" className="botones" >
+        favorites
+          </Button>
+
+
+          
+      <DropdownButton  align="end" id="nav-dropdown">
+        <div>
+        <Dropdown.Item className='drop'><Button variant="text" color="rojo" className="botones" onClick={() => logout()}>
+            My Profile
+            </Button></Dropdown.Item>
+            <Dropdown.Item className='drop'><Button variant="text" color="rojo" className="botones" onClick={() => logout()}>
+            Following
+            </Button></Dropdown.Item>
+            <Dropdown.Item className='drop'> <Button variant="text" color="rojo" className="botones" onClick={() => logout()}>
+           Settings
+            </Button></Dropdown.Item>
+        <Dropdown.Item className='drop'>
+          <Button variant="text" color="rojo" className="botones" onClick={() => logout()}>
+            Logout
+            </Button></Dropdown.Item>
+            </div>
+      </DropdownButton>
+
+</div>
              }
+
 
           </div>
     )
--- client/src/components/Login/Profile.jsx
+++ client/src/components/Login/Profile.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 import './Login.css'
+import './Profile.css'
 
 export default function Profile(){
 
     const {isAuthenticated,user} = useAuth0()
-    console.log(user)
     return(
             <div>
                 {
