f1a12fbca7066dac6665e0cd918aa16c2da3962e
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-11-13 17:00:08 -0300

    styles fixes

--- client/src/components/FilmCard/FilmCard.jsx
+++ client/src/components/FilmCard/FilmCard.jsx
@@ -1,27 +1,16 @@
 import { Link } from 'react-router-dom'
 import './FilmCard.css'
 
-
-
-const FilmCard = ({name, img, id, rating }) => {
- return (      
-
-<Link to={`/filmdetails/${id}`}>
-    <div className='Cardback'>
-
-        
-       
-        <div className='Imagen'>
-            <img width={190} height={240} src={img} alt={"IMG NOT FOUND"}/>
-        </div>
-
-
-
-
-    </div>
+const FilmCard = ({ name, img, id, rating }) => {
+    return (
+        <Link to={`/filmdetails/${id}`}>
+            <div className='Cardback'>
+                <div className='Imagen'>
+                    <img src={img} alt={"IMG NOT FOUND"} />
+                </div>
+            </div>
         </Link>
-        
-  )
+    )
 }
 
 export default FilmCard
