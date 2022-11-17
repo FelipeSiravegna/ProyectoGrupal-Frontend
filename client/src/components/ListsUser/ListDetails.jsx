c774270257939b57152f62a2b56b60e8567e5e7d
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-11-17 10:27:04 -0300

    deploy

--- client/src/components/ListsUser/ListDetails.jsx
+++ client/src/components/ListsUser/ListDetails.jsx
@@ -25,15 +25,15 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const ListDetails = () => {
 
-    const params = useParams();
-    const { idList } = params
-    const dispatch = useDispatch()
-    const lista = useSelector(state => (state.listDetails))
-    const usuario = lista.user?lista.user.id:null
-    const dueño = `${usuario}` === localStorage.id
-    const seguido = useSelector(state => (state.listFollowed))
-    const [estado, setEstado] = useState('')
-    const [estadito, setE] = useState('')
+  const params = useParams();
+  const { idList } = params
+  const dispatch = useDispatch()
+  const lista = useSelector(state => (state.listDetails))
+  const usuario = lista.user?lista.user.id:null
+  const dueño = `${usuario}` === localStorage.id
+  const seguido = useSelector(state => (state.listFollowed))
+  const [estado, setEstado] = useState('')
+  const [estadito, setE] = useState('')
 
 var items = document.getElementsByClassName("elnombresito")
 
@@ -43,9 +43,9 @@ let follow = variable? variable.filter(a=> a === idList):null
 let ayuda = follow?follow.length:null
 
 useEffect(()=>{
-  dispatch(listDetails(idList)) 
-  dispatch(followedList(localStorage.id)) 
-  items
+dispatch(listDetails(idList)) 
+dispatch(followedList(localStorage.id)) 
+items
 },[items.length, estado, dueño, usuario, estadito])
 
 console.log(estadito)
@@ -57,89 +57,87 @@ dispatch(followLists(e, idList))
 }
 
 function noSeguirLista (e){
-  dispatch(unFollowList(e, idList))
-  }
+dispatch(unFollowList(e, idList))
+}
 
 function removeFilm(b){
-  dispatch(removeMovieToList(idList, b))
-  setEstado(b)
+dispatch(removeMovieToList(idList, b))
+setEstado(b)
 }
 
 
-  return (
-    <div>
-      <img className='fondo'src={fondo} alt="" />
-    <NavbarP/>
+return (
+  <div>
+    <img className='fondo'src={fondo} alt="" />
+  <NavbarP/>
 <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="xl">
+    <CssBaseline />
+    <Container maxWidth="xl">
 
 
 
 {!dueño ? 
 <div className='followers'>
 {ayuda === 0 ? 
-        
-        
-        <Button variant='outlined' color='azul' onClick={()=>seguirLista(localStorage.id)} > <h className='letras'>FOLLOW</h>
+      
+      
+      <Button variant='outlined' color='azul' onClick={()=>seguirLista(localStorage.id)} > <h className='letras'>FOLLOW</h>
 <AddIcon onClick={()=>setE(false)} sx={{ fontSize: 50 }} color='azul'>  </AddIcon>
-        </Button> 
-        : <Button variant='outlined' color='rojo' onClick={()=>noSeguirLista(localStorage.id)}> <h className='letras'>UNFOLLOW</h>
-        <CloseIcon onClick={()=>setE(true)} sx={{ fontSize: 50 }} color='rojo'/>
-                </Button>}
-                </div>      
+      </Button> 
+      : <Button variant='outlined' color='rojo' onClick={()=>noSeguirLista(localStorage.id)}> <h className='letras'>UNFOLLOW</h>
+      <CloseIcon onClick={()=>setE(true)} sx={{ fontSize: 50 }} color='rojo'/>
+              </Button>}
+              </div>      
 : null}
 
 
-   {dueño ? 
-      <AddMovieList idList={idList} lista={lista}/>
-      : null}
+ {dueño ? 
+    <AddMovieList idList={idList} lista={lista}/>
+    : null}
 
-      <ListGroup as="ol" numbered>
+
+    <ListGroup as="ol" numbered>
 
 <h2 className='elnombre'><img className='loge' src={Logo}></img>{lista.name}</h2>     
 <h3 className='ladesc2'>By: {lista.user ? lista.user.username : null} </h3>
 <h3 className='ladesc'>{lista.description}</h3>
+
 {lista.movies? lista.movies.map(a => {return( 
-      <ListGroup.Item className='lalista'>
-      
+<div className='lalista2'>
+    <ListGroup.Item className='lalista'>
 <img className='listImg' src={a.image} alt="" />
 <h className="elnombresito">{a.name}</h>
-
-
 <Badge bg="none" pill className='ojo'> 
 <Link to={`/filmdetails/${a.id}`}>
 <RemoveRedEyeIcon color='azul'/>
 </Link>
-        </Badge>
+      </Badge>
 
 
 {dueño ? 
-        <Badge bg="none" pill className='ojo2'   >
-        <Button variant='outlined' color='rojo' onClick={()=>removeFilm(a.id)} >
+      <Badge bg="none" pill className='ojo2'   >
+      <Button variant='outlined' color='rojo' onClick={()=>removeFilm(a.id)} >
 <DeleteIcon color='rojo'/>
-        </Button>
-        </Badge>
-        : null}
-
-
-
-
-        
+      </Button>
+      </Badge>
+      : null}
+      
 
 </ListGroup.Item>
-)}): <div>
+</div>
+)}) : 
+<div>
 <h1 className='nd'>This list does not contain movies</h1>
 </div>
 }
-      </ListGroup>
-      </Container>
-    </React.Fragment>
+    </ListGroup>
+    </Container>
+  </React.Fragment>
 
 
 
-    </div>
-  )
+  </div>
+)
 }
 
 export default ListDetails
